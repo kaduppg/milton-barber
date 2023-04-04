bab25dc069cb187b6d933d35b1f4a39433578383
kaduppg <kaduppg@gmail.com>
2023-04-04 19:07:35 +1200

    adding footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -29,7 +29,7 @@ const navigation = [
   },
 ];
 
-export default function Footer() {
+export default function Footer() { 
   return (
     <footer className="bg-white">
       <div className="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
